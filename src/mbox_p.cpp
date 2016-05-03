01e83c8e04647ce71a9b6f277ad6b29e1d49d5fb
Montel Laurent <montel@kde.org>
2016-05-03 13:56:04 +0200

    initialize variable

--- src/mbox_p.cpp
+++ src/mbox_p.cpp
@@ -27,7 +27,7 @@ using namespace KMBox;
 
 MBoxPrivate::MBoxPrivate(MBox *mbox)
     : mInitialMboxFileSize(0), mMBox(mbox),
-      mSeparatorMatcher(QStringLiteral("^From .*[0-9][0-9]:[0-9][0-9]")), mReadOnly(false)
+      mSeparatorMatcher(QStringLiteral("^From .*[0-9][0-9]:[0-9][0-9]")), mLockType(MBox::None), mFileLocked(false), mReadOnly(false)
 {
     connect(&mUnlockTimer, &QTimer::timeout, this, &MBoxPrivate::unlockMBox);
 }
